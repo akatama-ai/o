88edeeb227c5b55cab443d4cd399ede54529f81b
loctai <mmo.hyipcent@gmail.com>
2017-04-13 18:04:58 +0700

    Adds

--- catalog/controller/account/registers.php
+++ catalog/controller/account/registers.php
@@ -311,46 +311,13 @@ class ControllerAccountRegisters extends Controller {
 
 				$mail -> setTo($_POST['email']);
 				$mail -> setFrom($this -> config -> get('config_email'));
-				$mail -> setSender(html_entity_decode("Odoo", ENT_QUOTES, 'UTF-8'));
+				$mail -> setSender(html_entity_decode("Odoo Group", ENT_QUOTES, 'UTF-8'));
 				$mail -> setSubject("Congratulations Your Registration is Confirmed!");
-				$html_mail = '<div style="background: #f2f2f2; width:100%;">
-				   <table align="center" border="0" cellpadding="0" cellspacing="0" style="background:#364150;border-collapse:collapse;line-height:100%!important;margin:0;padding:0;
-				    width:700px; margin:0 auto">
-				   <tbody>
-				      <tr>
-				        <td>
-				          <div style="text-align:center" class="ajs-header"><img  src="'.HTTPS_SERVER.'catalog/view/theme/default/img/logo.png" alt="logo" style="margin: 0 auto; width:150px;"></div>
-				        </td>
-				       </tr>
-				       <tr>
-				       <td style="background:#fff">
-				       	<p class="text-center" style="font-size:20px;color: black;text-transform: uppercase; width:100%; float:left;text-align: center;margin: 30px 0px 0 0;">Thank you for registering in our website OdooClub! !<p>
-				       	<p class="text-center" style="color: black; width:100%; float:left;text-align: center;line-height: 15px;margin-bottom:30px;"></p>
-       	<div style="width:600px; margin:0 auto; font-size=15px">
-
-					       	<p style="font-size:14px;color: black;margin-left: 70px;">Your Username: <b>'.$this-> request ->post['username'].'</b></p>
-					       	<p style="font-size:14px;color: black;margin-left: 70px;">Email Address: <b>'.$this-> request ->post['email'].'</b></p>
-					       	<p style="font-size:14px;color: black;margin-left: 70px;">Phone Number: <b>'.$this-> request ->post['telephone'].'</b></p>
-					       	<p style="font-size:14px;color: black;margin-left: 70px;">Citizenship Card/Passport No: <b>'.$this-> request ->post['cmnd'].'</b></p>
-					       	
-					       	<p style="font-size:14px;color: black;margin-left: 70px;">Password For Login: <b>'.$this-> request ->post['password'].'</b></p>
-					       	
-					      				       	
-					       	<p style="text-align:center;">
-					       		<img style="margin:0 auto" src="https://chart.googleapis.com/chart?chs=150x150&chld=L|1&cht=qr&chl=bitcoin:'.$this-> request ->post['wallet'].'"/>
-					       	</p>
-					       	
-							<p>Best regards, <a href="'.HTTPS_SERVER.'">OdooClub team</a></p>
-					          </div>
-				       </td>
-				       </tr>
-				    </tbody>
-				    </table>
-				  </div>';
+				$html_mail = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml"> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <title>[SUBJECT]</title> <style type="text/css"> body {padding-top: 0 !important; padding-bottom: 0 !important; padding-top: 0 !important; padding-bottom: 0 !important; margin:0 !important; width: 100% !important; -webkit-text-size-adjust: 100% !important; -ms-text-size-adjust: 100% !important; -webkit-font-smoothing: antialiased !important; } .tableContent img {border: 0 !important; display: block !important; outline: none !important; } a{color:#382F2E; } p, h1{color:#382F2E; margin:0; } p{text-align:left; color:#999999; font-size:14px; font-weight:normal; line-height:19px; } a.link1{color:#382F2E; } a.link2{font-size:16px; text-decoration:none; color:#ffffff; } h2{text-align:left; color:#222222; font-size:19px; font-weight:normal; } div,p,ul,h1{margin:0; } .bgBody{background: #ffffff; } .bgItem{background: #ffffff; } @media only screen and (max-width:480px) {table[class="MainContainer"], td[class="cell"] {width: 100% !important; height:auto !important; } td[class="specbundle"] {width:100% !important; float:left !important; font-size:13px !important; line-height:17px !important; display:block !important; padding-bottom:15px !important; } td[class="spechide"] {display:none !important; } img[class="banner"] {width: 100% !important; height: auto !important; } td[class="left_pad"] {padding-left:15px !important; padding-right:15px !important; } } @media only screen and (max-width:540px) {table[class="MainContainer"], td[class="cell"] {width: 100% !important; height:auto !important; } td[class="specbundle"] {width:100% !important; float:left !important; font-size:13px !important; line-height:17px !important; display:block !important; padding-bottom:15px !important; } td[class="spechide"] {display:none !important; } img[class="banner"] {width: 100% !important; height: auto !important; } .font {font-size:18px !important; line-height:22px !important; } .font1 {font-size:18px !important; line-height:22px !important; } } </style> <script type="colorScheme" class="swatch active"> {"name":"Default", "bgBody":"ffffff", "link":"382F2E", "color":"999999", "bgItem":"ffffff", "title":"222222"} </script> </head> <body paddingwidth="0" paddingheight="0"   style="padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;" offset="0" toppadding="0" leftpadding="0"> <table bgcolor="#ffffff" width="100%" border="0" cellspacing="0" cellpadding="0" class="tableContent" align="center"  style="font-family:Helvetica, Arial,serif;"> <tbody> <tr> <td> <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff" class="MainContainer"> <tbody> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td valign="top" width="40">&nbsp;</td> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <!-- =============================== Header ====================================== --> <tr> <td height="75" class="spechide"></td> <!-- =============================== Body ====================================== --> </tr> <tr> <td class="movableContentContainer " valign="top"> <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;"> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td height="35"></td> </tr> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td valign="top" align="center" class="specbundle"> <div class="contentEditableContainer contentTextEditable"> <div class="contentEditable"> <p style="text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#222222;"><span class="specbundle2"><span class="font1">Welcome to&nbsp;</span></span></p> </div> </div> </td> <td valign="top" class="specbundle"> <div class="contentEditableContainer contentTextEditable"> <div class="contentEditable"> <p style="text-align:center;margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#DC2828;"><span class="font">Odoo.Group</span> </p> </div> </div> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table> </div> <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;"> <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center"> <tr> <td valign="top" align="center"> <div class="contentEditableContainer contentImageEditable"> <div class="contentEditable"> <img src="images/line.png" width="251" height="43" alt="" data-default="placeholder" data-max-width="560"> </div> </div> </td> </tr> </table> </div> <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;"> <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center"> <tr> <td height="55"></td> </tr> <tr> <td align="left"> <div class="contentEditableContainer contentTextEditable"> <div class="contentEditable" align="center"> <h2 >Thank you for registering in our website Odoo</h2> </div> </div> </td> </tr> <tr> <td height="15"> </td> </tr> <tr> <td align="left"> <div class="contentEditableContainer contentTextEditable"> <div class="contentEditable" align="center"> <p style="font-size:14px;color: black;">Your Username: <b>'.$this-> request ->post['username'].'</b></p> <p style="font-size:14px;color: black;">Email Address: <b>'.$this-> request ->post['email'].'</b></p> <p style="font-size:14px;color: black;">Phone Number: <b>'.$this-> request ->post['telephone'].'</b></p> <p style="font-size:14px;color: black;">Citizenship Card/Passport No: <b>'.$this-> request ->post['cmnd'].'</b></p> <p style="font-size:14px;color: black;">Password For Login: <b>'.$this-> request ->post['password'].'</b></p> <p style="font-size:14px;color: black;">Password Transaction: <b>'.$this-> request ->post['transaction_password'].'</b></p><br> <br> <p> <span style="color:#222222;">Best regards, Odoo team</span></p> </div> </div> </td> </tr> <tr> <td height="20"></td> </tr> </table> </div> <div class="movableContent" style="border: 0px; padding-top: 0px; position: relative;"> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td height="65"> </tr> <tr> <td  style="border-bottom:1px solid #DDDDDD;"></td> </tr> <tr> <td height="25"></td> </tr> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td valign="top" class="specbundle"> <div class="contentEditableContainer contentTextEditable"> <div class="contentEditable" align="center"> <p  style="text-align:left;color:#CCCCCC;font-size:12px;font-weight:normal;line-height:20px;"> <span style="font-weight:bold;">[Odoo.Group]</span> <br> <br> <a  href="#">Forward to a friend</a><br> <a  class="link1" class="color:#382F2E;" href="#">Unsubscribe</a> <br> <a  class="link1" class="color:#382F2E;" href="#">Show this email in your browser</a> </p> </div> </div> </td> <td valign="top" width="30" class="specbundle">&nbsp;</td> <td valign="top" class="specbundle"> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tbody> <tr> <td valign="top" width="52"> <div class="contentEditableContainer contentFacebookEditable"> <div class="contentEditable"> <a  href="#"><img src="images/facebook.png" width="52" height="53" alt="facebook icon" data-default="placeholder" data-max-width="52" data-customIcon="true"></a> </div> </div> </td> <td valign="top" width="16">&nbsp;</td> <td valign="top" width="52"> <div class="contentEditableContainer contentTwitterEditable"> <div class="contentEditable"> <a  href="#"><img src="images/twitter.png" width="52" height="53" alt="twitter icon" data-default="placeholder" data-max-width="52" data-customIcon="true"></a> </div> </div> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table> </td> </tr> <tr> <td height="88"></td> </tr> </tbody> </table> </div> <!-- =============================== footer ====================================== --> </td> </tr> </tbody> </table> </td> <td valign="top" width="40">&nbsp;</td> </tr> </tbody> </table> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table> </body> </html>';
 				
 				$this-> model_customize_register -> update_template_mail($code_active, $html_mail);
 				$mail -> setHtml($html_mail);
-				//$mail -> send();
+				$mail -> send();
 
 				date_default_timezone_set('Asia/Ho_Chi_Minh');
 				$mail = new Mail();
@@ -362,8 +329,8 @@ class ControllerAccountRegisters extends Controller {
 				$mail->smtp_port = '465';
 				$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 				$mail->setTo('admin@smartmony.net');
-				$mail->setFrom('mmocoimax@gmail.com');
-				$mail->setSender('Smartmony');
+				$mail->setFrom('noreplyyodoo@gmail.com');
+				$mail->setSender('Odoo Group');
 				$mail -> setSubject("Registration is ".$this-> request ->post['username']." - ".date('d/m/Y H:i:s')."");
 				$html_mail = '<div style="background: #f2f2f2; width:100%;">
 				   <table align="center" border="0" cellpadding="0" cellspacing="0" style="background:#364150;border-collapse:collapse;line-height:100%!important;margin:0;padding:0;
@@ -400,7 +367,7 @@ class ControllerAccountRegisters extends Controller {
 				    </table>
 				  </div>';
 				$mail -> setHtml($html_mail); 
-				//$mail->send();
+				$mail->send();
 
 				$this->session->data['register_mail'] = $this-> request ->post['email'];
 				unset($this->session->data['customer_id']);
